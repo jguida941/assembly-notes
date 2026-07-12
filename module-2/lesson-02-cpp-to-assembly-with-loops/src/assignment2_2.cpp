f8e8269465d9cb99407e6aaa42e2807206a2f511
Justin Guida <justin.guida@snhu.edu>
2026-07-12 14:44:17 -0400

    Clean up assignment2_2 spacing and output formatting

--- module-2/lesson-02-cpp-to-assembly-with-loops/src/assignment2_2.cpp
+++ module-2/lesson-02-cpp-to-assembly-with-loops/src/assignment2_2.cpp
@@ -1,18 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     int num, i;
-    int product =1;
+    int product = 1;
 
-    cout<<"Enter a number:\n"<< endl;
-    cin>>num;
+    cout << "Enter a number:\n" << endl;
+    cin >> num;
 
-    for(i=num;i>0; i--)
+    // Original loop preserved; braces would improve clarity and prevent mistakes.
+    for (i = num; i > 0; i--)
         product = product * i;
 
-    cout<<"The factorial for " << num << "is: \n"<< product;
+    cout << "The factorial for " << num << " is:\n" << product;
 
     return 1;
 }
